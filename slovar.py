905a19a047ffe8857f9449988cc20dac63e8a78c
VadymDrebot <72618090+VadymDrebot@users.noreply.github.com>
2020-10-24 16:51:48 +0300

    my projects

--- slovar.py
+++ slovar.py
@@ -1,3 +1,12 @@
-sl={1:'first',2:222}
-print(sl)
+sl1={}
+sl2={}
+#tel=input("telef")
+
+sl={'a': '123', 'b':'asd'}
+if 'a' in sl:
+    sl1['a']=sl['a']
+
+print(sl1)
+if 'b' in sl:
+    sl1['b']=sl['b']
 
